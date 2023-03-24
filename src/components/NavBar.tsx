887800dcca46b3518124691729ad0ca827bd5738
tuppers360 <tuppers360@live.co.uk>
2023-03-24 20:10:54 +0000

    reformat nav

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,23 +1,32 @@
 'use client';
 
-import { cn } from "@/lib/utils";
-import { Popover, Transition } from "@headlessui/react";
-import { Bars3Icon, BellIcon, ShieldCheckIcon, XMarkIcon } from "@heroicons/react/20/solid";
-import { EnvelopeIcon, MapPinIcon, PhoneIcon } from "@heroicons/react/24/outline";
-import { Oswald } from "next/font/google";
-import Link from "next/link";
-import { Fragment } from "react";
+import { cn } from '@/lib/utils';
+import { Popover, Transition } from '@headlessui/react';
+import {
+  Bars3Icon,
+  BellIcon,
+  ShieldCheckIcon,
+  XMarkIcon,
+} from '@heroicons/react/20/solid';
+import {
+  EnvelopeIcon,
+  MapPinIcon,
+  PhoneIcon,
+} from '@heroicons/react/24/outline';
+import { Oswald } from 'next/font/google';
+import Link from 'next/link';
+import { Fragment } from 'react';
 
-const oswald = Oswald({ subsets: ['latin'], weight: ['700'] })
+const oswald = Oswald({ subsets: ['latin'], weight: ['700'] });
 
-type NavBarProps = { name: string; href: string }[]
+type NavBarProps = { name: string; href: string }[];
 
 function NavBar({ bottomNavigation }: { bottomNavigation: NavBarProps }) {
   return (
-    <Popover as="header" className="bg-white shadow">
+    <Popover as="header" className="bg-slate-100/50">
       {({ open }) => (
         <>
-          <div className="mx-auto max-w-7xl px-2 sm:px-4 lg:divide-y lg:divide-gray-200 lg:pb-8  lg:px-8">
+          <div className="mx-auto max-w-7xl px-2 sm:px-4 lg:divide-y lg:divide-gray-200 lg:px-8  lg:pb-8">
             <div className="relative flex h-24">
               <div className="flex flex-1 items-center justify-between lg:items-stretch lg:justify-start">
                 <div className="flex flex-shrink-0 items-center">
@@ -66,7 +75,7 @@ function NavBar({ bottomNavigation }: { bottomNavigation: NavBarProps }) {
               </div>
 
               <div className="hidden gap-x-5 lg:relative lg:z-10 lg:ml-8 lg:flex lg:items-center">
-                <div className="items-center font-medium text-indigo-700 sm:inline-flex">
+                <div className="items-center font-medium text-slate-600 sm:inline-flex">
                   <span className="rounded-lg bg-amber-200 p-3">
                     <MapPinIcon className="h-6 w-6" aria-hidden="true" />
                   </span>
@@ -77,12 +86,9 @@ function NavBar({ bottomNavigation }: { bottomNavigation: NavBarProps }) {
                     </p>
                   </div>
                 </div>
-                <div className="items-center font-medium text-indigo-700 sm:inline-flex">
+                <div className="items-center font-medium text-slate-600 sm:inline-flex">
                   <span className="rounded-lg bg-red-200 p-3">
-                    <EnvelopeIcon
-                      className="h-6 w-6"
-                      aria-hidden="true"
-                    />
+                    <EnvelopeIcon className="h-6 w-6" aria-hidden="true" />
                   </span>
                   <div className="ml-4 flex flex-col">
                     <h5 className="font-bold">Email</h5>
@@ -91,7 +97,7 @@ function NavBar({ bottomNavigation }: { bottomNavigation: NavBarProps }) {
                     </p>
                   </div>
                 </div>
-                <div className="items-center font-medium text-indigo-700 sm:inline-flex">
+                <div className="items-center font-medium text-slate-600 sm:inline-flex">
                   <span className="rounded-lg bg-indigo-200 p-3">
                     <PhoneIcon className="h-6 w-6" aria-hidden="true" />
                   </span>
@@ -110,7 +116,7 @@ function NavBar({ bottomNavigation }: { bottomNavigation: NavBarProps }) {
                 <Link
                   key={item.name}
                   href={item.href}
-                  className="relative z-10 block w-fit text-lg font-medium text-indigo-700 after:absolute after:block after:h-[3px] after:w-full after:origin-left after:scale-x-0 after:bg-indigo-500 after:transition after:duration-300 after:content-[''] after:hover:scale-x-100"
+                  className="relative z-10 block w-fit text-base font-medium text-slate-600 after:absolute after:block after:h-[3px] after:w-full after:origin-left after:scale-x-0 after:bg-indigo-500 after:transition after:duration-300 after:content-[''] after:hover:scale-x-100"
                 >
                   {item.name}
                 </Link>
@@ -148,10 +154,7 @@ function NavBar({ bottomNavigation }: { bottomNavigation: NavBarProps }) {
                     <div className="bg-indigo-100/50 pt-3 pb-2">
                       <div className="flex items-center justify-between px-4">
                         <div>
-                          <Link
-                            className="inline-flex items-center"
-                            href="/"
-                          >
+                          <Link className="inline-flex items-center" href="/">
                             <ShieldCheckIcon
                               className="h-8 w-auto text-amber-500"
                               aria-hidden="true"
@@ -179,10 +182,7 @@ function NavBar({ bottomNavigation }: { bottomNavigation: NavBarProps }) {
                         <div className="-mr-2">
                           <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-cyan-500">
                             <span className="sr-only">Close menu</span>
-                            <XMarkIcon
-                              className="h-6 w-6"
-                              aria-hidden="true"
-                            />
+                            <XMarkIcon className="h-6 w-6" aria-hidden="true" />
                           </Popover.Button>
                         </div>
                       </div>
@@ -204,13 +204,8 @@ function NavBar({ bottomNavigation }: { bottomNavigation: NavBarProps }) {
                           type="button"
                           className="ml-auto flex-shrink-0 rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-cyan-500 focus:ring-offset-2"
                         >
-                          <span className="sr-only">
-                            View notifications
-                          </span>
-                          <BellIcon
-                            className="h-6 w-6"
-                            aria-hidden="true"
-                          />
+                          <span className="sr-only">View notifications</span>
+                          <BellIcon className="h-6 w-6" aria-hidden="true" />
                         </button>
                       </div>
                     </div>
