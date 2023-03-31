0025260d7ba279b14512d5e1643cb4eeead1b53c
tuppers360 <tuppers360@live.co.uk>
2023-03-31 15:47:56 +0100

    add sanity unsplash plugin

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,4 +1,5 @@
 import { schemaTypes } from '@/app/(sanity)/admin/schema';
+import { unsplashImageAsset } from 'sanity-plugin-asset-source-unsplash';
 import { deskTool } from 'sanity/desk';
 
 export const config = {
@@ -6,7 +7,7 @@ export const config = {
   dataset: 'production',
   title: 'Sanity Studio',
   basePath: '/admin',
-  plugins: [deskTool()],
+  plugins: [deskTool(), unsplashImageAsset()],
   schema: {
     types: schemaTypes,
   },
