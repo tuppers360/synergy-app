b2133d9c7eec6ec8d6784e961bb1ad86b246153d
tuppers360 <tuppers360@live.co.uk>
2023-04-23 16:03:05 +0100

    add function to get policies

--- src/lib/sanity/sanity.utils.ts
+++ src/lib/sanity/sanity.utils.ts
@@ -1,5 +1,6 @@
 import { createClient, groq } from 'next-sanity';
 import { Corgi } from '../../../types/corgi';
+import { Policy } from '../../../types/policy';
 import clientConfig from './config/client.config';
 
 export async function getCorgis(): Promise<Corgi[]> {
@@ -10,6 +11,17 @@ export async function getCorgis(): Promise<Corgi[]> {
         name,
         "slug": slug.current,
         "image": image.asset->url,
+        content
+      }`
+  );
+}
+
+export async function getPolicies(): Promise<Policy[]> {
+  return createClient(clientConfig).fetch(
+    groq`*[_type == "policy"]{
+        _id,
+        _createdAt,
+        name,
         content
       }`
   );
